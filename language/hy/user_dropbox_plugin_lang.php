bcdf2adf1e7b7e6f9e2d2d4533241d0a39a89f40
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:10:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Ընտրման տուփից plugin ապահովում է մուտքի վերահսկման թույլ է տալիս օգտվողներին ստեղծել տուն թղթապանակը Թղթապանակ Sync է ամպի վրա հիմնված ընտրման տուփից ծառայությանը.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Օգտվող քաղաքականություն';
+$lang['user_dropbox_plugin_user'] = 'Dropbox Օգտվող';
