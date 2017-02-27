923fc92ae95b8dbaa3b53ac4655181ac273154f5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:10:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox plugin omogućuje kontrolu pristupa kako bi se omogućilo korisnicima da stvori home direktorij folder sync na oblaku Dropbox usluga.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Politika korisnika';
+$lang['user_dropbox_plugin_user'] = 'Dropbox korisnika';
