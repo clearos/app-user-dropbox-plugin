259e0ea81ea9eb4376c8d858639582e309bfa6f1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:21:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'דראָפּבאָקס פּלוגין גיט צוטריט קאָנטראָל צו לאָזן ניצערס צו שאַפֿן אַ היים Directory טעקע סינק צו די וואָלקן-באזירט דראָפּבאָקס דינסט.';
+$lang['user_dropbox_plugin_app_name'] = 'דראָפּבאָקס באַניצער פּאָליטיק';
+$lang['user_dropbox_plugin_user'] = 'דראָפּבאָקס אַוואַטאַר';
