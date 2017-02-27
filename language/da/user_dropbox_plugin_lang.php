15d2133c3cec115835a653580ea28ea37e91561e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:11:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox plugin giver adgangskontrol at give brugerne mulighed for at skabe et hjem bibliotek mappe synkronisering til den skybaserede Dropbox tjeneste.';
+$lang['user_dropbox_plugin_app_name'] = 'Bruger Politik Dropbox';
+$lang['user_dropbox_plugin_user'] = 'Dropbox Bruger';
