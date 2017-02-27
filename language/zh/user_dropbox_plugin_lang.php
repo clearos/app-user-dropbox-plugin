3a19d37d682cc5995e15c9697c65f726d67f8996
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:11:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox的插件提供了访问控制，允许用户创建一个主目录文件夹同步到基于云的服务的Dropbox。';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox的用户策略';
+$lang['user_dropbox_plugin_user'] = 'Dropbox的用户';
