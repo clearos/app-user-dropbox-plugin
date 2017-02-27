2545f641969d95c0a8b95def69cc221ac19a218b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:21:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox Plugin cung cấp điều khiển truy cập để cho phép người dùng tạo ra một thư mục thư mục đồng bộ hóa với dịch vụ Dropbox dựa trên đám mây.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Chính sách tài';
+$lang['user_dropbox_plugin_user'] = 'Dropbox tài';
