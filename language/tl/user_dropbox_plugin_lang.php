690667880bed85493cf8980f6ae0b11cbdd11c71
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:20:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox plugin ay nagbibigay ng access control upang payagan ang mga gumagamit upang lumikha ng isang tahanan na direktoryo folder sync sa cloud-based na Dropbox serbisyo.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Patakaran User';
+$lang['user_dropbox_plugin_user'] = 'Dropbox User';
