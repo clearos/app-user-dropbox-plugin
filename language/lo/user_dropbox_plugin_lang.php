e431aea44cec7da1942a9e25329d0d1d8763bef7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:15:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'plugin Dropbox ໃຫ້ການຄວບຄຸມການເຂົ້າເຖິງການອະນຸຍາດໃຫ້ຜູ້ໃຊ້ເພື່ອສ້າງເປັນ sync ໂຟເດີລະບົບເຮືອນເພື່ອຟັງທີ່ອີງໃສ່ການບໍລິການ Dropbox.';
+$lang['user_dropbox_plugin_app_name'] = 'ນະໂຍບາຍຜູ້ໃຊ້ Dropbox';
+$lang['user_dropbox_plugin_user'] = 'ຜູ້ໃຊ້ Dropbox';
