0e4fc43ece5f87e21c706423d7e4cfd0afdb305a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:20:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'ปลั๊กอิน Dropbox ให้การควบคุมการเข้าถึงที่จะอนุญาตให้ผู้ใช้สามารถสร้างโฟลเดอร์ซิงค์ไดเรกทอรีบ้านกับบริการ Dropbox เมฆตาม';
+$lang['user_dropbox_plugin_app_name'] = 'นโยบายของผู้ใช้ Dropbox';
+$lang['user_dropbox_plugin_user'] = 'ผู้ใช้ Dropbox';
