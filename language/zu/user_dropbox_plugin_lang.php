7f3afeed3e389a2cc14dd1f3b6f471b22d48cd02
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:21:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Eyinqolobane plugin inikeza control ukufinyelela ukuvumela abasebenzisi ukudala ekhaya lwemibhalo ukuvumelanisa ifolda efwini-based eyinqolobane service.';
+$lang['user_dropbox_plugin_app_name'] = 'Eyinqolobane yemfihlo Umsebenzisi';
+$lang['user_dropbox_plugin_user'] = 'eyinqolobane Umsebenzisi';
