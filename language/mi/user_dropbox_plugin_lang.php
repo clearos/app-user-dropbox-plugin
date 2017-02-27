9c95594a2963edd4213f3aa586bbff1dc3eddd87
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:16:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'whakarato Dropbox mono mana uru ki te tukua ngā kaiwhakamahi ki te waihanga i tētahi kōpaki tukutahi whaiaronga kāinga ki te mahi Dropbox hāngai-kapua.';
+$lang['user_dropbox_plugin_app_name'] = 'Kaupapahere Kaiwhakamahi Dropbox';
+$lang['user_dropbox_plugin_user'] = 'Kaiwhakamahi Dropbox';
