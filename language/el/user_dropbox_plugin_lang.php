cb960a6428a11d9f52eaed57cf487f6517eb91d2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:12:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox plugin παρέχει έλεγχο πρόσβασης για να επιτρέψει στους χρήστες να δημιουργήσουν ένα συγχρονισμό φάκελο Αρχική στο cloud-based υπηρεσία Dropbox.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Πολιτική χρήσης';
+$lang['user_dropbox_plugin_user'] = 'Dropbox χρήστη';
