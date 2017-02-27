372fd96c7566105a082fdf23ead8fc5652424400
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:14:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox плагин пайдаланушылар бұлтқа негізделген қызметке Dropbox үй каталогы қалта синхрондауды құруға мүмкіндік беру үшін қол бақылауды қамтамасыз етеді.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Пайдаланушы саясаты';
+$lang['user_dropbox_plugin_user'] = 'Dropbox Пайдаланушы';
