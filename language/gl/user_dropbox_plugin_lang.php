06ccf856759cfd058d3a1a5b57750b3ba7955c3d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:12:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox plugin ofrece control de acceso para permitir que os usuarios creen un cartafol de sincronización inicial para o servizo Dropbox baseado na nube.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Política de usuario';
+$lang['user_dropbox_plugin_user'] = 'Dropbox usuario';
