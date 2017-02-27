3678f765aabb9ff24e423d47fb2179198cd1ebd8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:18:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Дропбок језичак обезбеђује контролу приступа да омогући корисницима да створе Хоме Дирецтори фолдер синц на облаку сервис Дропбок.';
+$lang['user_dropbox_plugin_app_name'] = 'Дропбок Корисник Политика';
+$lang['user_dropbox_plugin_user'] = 'дропбок Корисник';
