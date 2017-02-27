5e7143484a8c100c3a2912abf81f707add255900
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:17:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'پلاگین Dropbox به فراهم می کند کنترل دسترسی به کاربران اجازه می دهد برای ایجاد یک پوشه همگام دایرکتوری خانگی به سرویس Dropbox مبتنی بر ابر.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox به خطمشی کاربر';
+$lang['user_dropbox_plugin_user'] = 'Dropbox به کاربر';
