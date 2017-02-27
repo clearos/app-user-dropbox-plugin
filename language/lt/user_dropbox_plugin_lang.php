06f5a660e562f667a7c1a846215ff8c894fa2752
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:15:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'ZMI įskiepiai suteikia prieigos kontrolės, kad vartotojai galėtų sukurti namų aplanko aplanko sinchronizacija į debesis pagrįstas Dropbox paslauga.';
+$lang['user_dropbox_plugin_app_name'] = 'ZMI Vartotojas politika';
+$lang['user_dropbox_plugin_user'] = 'ZMI Vartotojas';
