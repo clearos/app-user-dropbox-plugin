254fa8baa0d4a9ab90dbc3c978640bf584807e59
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:12:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox lisäosa toteuttaa kulunvalvonta, jotta käyttäjät voivat luoda kotihakemiston kansion synkronointi pilvipohjaisen Dropbox palvelu.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox käyttäjäkäytäntö';
+$lang['user_dropbox_plugin_user'] = 'Dropbox Käyttäjä';
