2d0510092bae598833ac30bd76003a34d94a90ee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:14:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox მოდული უზრუნველყოფს ხელმისაწვდომობის კონტროლის საშუალებას მისცემს მომხმარებლებს შექმნათ სახლის დირექტორია საქაღალდეში სინქრონიზაციის Cloud დაფუძნებული Dropbox მომსახურება.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox User პოლიტიკა';
+$lang['user_dropbox_plugin_user'] = 'Dropbox მომხმარებელი';
