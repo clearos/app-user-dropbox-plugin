3ca51c02f70730df7c428a22ba410157e88d8741
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:10:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'يوفر دروببوإكس البرنامج المساعد التحكم في الوصول للسماح للمستخدمين لإنشاء تزامن مجلد الدليل موطنا لخدمة دروببوإكس القائمة على السحابة.';
+$lang['user_dropbox_plugin_app_name'] = 'دروببوإكس نهج المستخدم';
+$lang['user_dropbox_plugin_user'] = 'دروببوإكس العضو';
