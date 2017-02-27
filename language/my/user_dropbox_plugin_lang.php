215cb1bf41ed5337aa6e5dcf1196cbd9455906dc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:11:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox ကို plugin ကိုအသုံးပြုသူများသည်မိုဃ်းတိမ်-based Dropbox ကိုဝန်ဆောင်မှုတစ်ခုနေအိမ် directory ကို folder ကိုထပ်တူပြုခြင်းကိုဖန်တီးရန်ခွင့်ပြုမှ access ကိုထိန်းချုပ်ပေးသည်။';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox ကိုအသုံးပြုသူပေါ်လစီ';
+$lang['user_dropbox_plugin_user'] = 'Dropbox ကိုအသုံးပြုသူတို့၏';
