e324413126353d3f13785cfe55fe20db0ba3e2f6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:19:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox plugin nyadiakeun kadali aksés pikeun ngidinan pamaké pikeun nyieun diréktori imah polder singkronisasi ka ladenan Dropbox basis awan.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Kawijakan Pamaké';
+$lang['user_dropbox_plugin_user'] = 'Dropbox pamaké';
