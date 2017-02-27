64e9422f79526c33c821e7e9af4ee1fccb6669dc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:14:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox បានផ្តល់នូវការត្រួតពិនិត្យកម្មវិធីជំនួយការចូលដំណើរការទៅអនុញ្ញាតឱ្យអ្នកប្រើធ្វើសមកាលកម្មថតបង្កើតបានជាថតផ្ទះទៅសេវាកម្ម Dropbox ពពកដែលមានមូលដ្ឋាន។';
+$lang['user_dropbox_plugin_app_name'] = 'គោលនយោបាយអ្នកប្រើប្រាស់ Dropbox';
+$lang['user_dropbox_plugin_user'] = 'អ្នកប្រើ Dropbox';
