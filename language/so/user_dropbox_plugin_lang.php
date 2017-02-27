77e58c3dd0e45c614e2386548d73b00b36236964
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:18:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'plugin DropBox bixisaa helitaanka ah in ay u oggolaadaan dadka isticmaala si ay u abuuraan hagaagsan folder buugga guri adeeg DropBox daruur ku salaysan.';
+$lang['user_dropbox_plugin_app_name'] = 'Siyaasadda User DropBox';
+$lang['user_dropbox_plugin_user'] = 'User DropBox';
