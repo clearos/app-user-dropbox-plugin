4f1758978a6a514b4641c8bec958be9beb5cc276
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:20:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox eklentisi, kullanıcıların bulut tabanlı Dropbox hizmet için bir ev dizini klasör senkronizasyonu oluşturmak için izin erişim kontrolü sağlar.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Kullanıcı İlkesi';
+$lang['user_dropbox_plugin_user'] = 'dropbox Kullanıcı';
