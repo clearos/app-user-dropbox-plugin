35b88b88f4ea27f533baafb2d24d82f8e1715163
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:21:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox plagin foydalanuvchilar bulutga asoslangan Dropbox xizmatiga bir uy katalog papka sinxronlashtirishni yaratish uchun ruxsat berish uchun erkin foydalanish nazorat etadi.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Foydalanuvchi siyosati';
+$lang['user_dropbox_plugin_user'] = 'Dropbox Foydalanuvchi';
