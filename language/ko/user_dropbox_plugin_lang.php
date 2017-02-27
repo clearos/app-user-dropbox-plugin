bbca8df56086120ee0a3829ddb48dc48cbb9c8fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:15:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = '보관 용 플러그인은 사용자가 클라우드 기반 보관 서비스에 홈 디렉토리 폴더 동기화를 만들 수 있도록 액세스 제어를 제공합니다.';
+$lang['user_dropbox_plugin_app_name'] = '보관 용 사용자 정책';
+$lang['user_dropbox_plugin_user'] = '드롭 박스 사용자';
