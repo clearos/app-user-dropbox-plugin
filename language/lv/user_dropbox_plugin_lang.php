ee84777ab21dcb61edb0ec08a8d521555c4248ad
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:15:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox spraudnis nodrošina piekļuves kontroles, kas ļauj lietotājiem izveidot mājas direktoriju mapju sinhronizācija uz mākoņa arhitektūras Dropbox pakalpojumu.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox User politika';
+$lang['user_dropbox_plugin_user'] = 'Dropbox User';
