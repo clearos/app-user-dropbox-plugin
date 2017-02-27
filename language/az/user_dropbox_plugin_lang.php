2861b272d9fea13a7b9f00c32d073af21d30606c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:10:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox plugin istifadəçi cloud-based Dropbox xidmət üçün Baş qovluq sync yaratmaq üçün imkan kontrol verir.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox İstifadəçi Siyasət';
+$lang['user_dropbox_plugin_user'] = 'Dropbox İstifadəçi';
