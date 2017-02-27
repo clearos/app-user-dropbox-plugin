c9dcf6649aa0a498f645b4190c33bd228a37aecc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:17:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox ਪਲੱਗਇਨ ਉਪਭੋਗੀ ਬੱਦਲ-ਅਧਾਰਿਤ ਡ੍ਰੌਪਬਾਕਸ ਸੇਵਾ ਕਰਨ ਲਈ ਇੱਕ ਘਰ ਡਾਇਰੈਕਟਰੀ ਫੋਲਡਰ ਨੂੰ ਸਮਕਾਲੀ ਬਣਾਉਣ ਲਈ ਸਹਾਇਕ ਹੈ, ਤੱਕ ਪਹੁੰਚ ਕੰਟਰੋਲ ਮੁਹੱਈਆ ਕਰਦਾ ਹੈ.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox ਯੂਜ਼ਰ ਨੀਤੀ';
+$lang['user_dropbox_plugin_user'] = 'Dropbox ਯੂਜ਼ਰ';
