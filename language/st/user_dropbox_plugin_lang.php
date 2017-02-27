36315228f413c31dff492f225cbb9578c09744f4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:19:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox plugin fana ka phihlello ho laola maikutlo a lumella basebelisi ho bopa ea lehae ea dibaka foldara kgokagantswe ho leru thehiloeng Dropbox tšebeletso.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Policy User';
+$lang['user_dropbox_plugin_user'] = 'Dropbox User';
