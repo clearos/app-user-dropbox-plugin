731b81a3887b83da86789503230173d642210329
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:16:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox plugin menyediakan kawalan akses untuk membolehkan pengguna untuk mewujudkan direktori rumah folder penyegerakan kepada perkhidmatan Dropbox berasaskan awan.';
+$lang['user_dropbox_plugin_app_name'] = 'Dasar Pengguna Dropbox';
+$lang['user_dropbox_plugin_user'] = 'Dropbox Pengguna';
