04bfbb0e859b07aad4125baa07efcbbc5b21b768
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:21:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'ategyn Dropbox yn darparu rheoli mynediad i alluogi defnyddwyr i greu cyfeiriadur cartref ffolder cydamseru at y gwasanaeth Dropbox seiliedig cwmwl.';
+$lang['user_dropbox_plugin_app_name'] = 'Polisi Defnyddwyr Dropbox';
+$lang['user_dropbox_plugin_user'] = 'Defnyddiwr Dropbox';
