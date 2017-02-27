2e3977cbcef9032ef0f1f46a5bc5c3f31ff707e9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:18:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'plugin Dropbox oferă un control de acces, pentru a permite utilizatorilor să creeze un dosar de sincronizare director acasă la serviciul Dropbox cloud-based.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Politica de utilizare';
+$lang['user_dropbox_plugin_user'] = 'dropbox utilizator';
