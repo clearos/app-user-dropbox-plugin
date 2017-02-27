ef5200c7e48812409a87c676291d717876eb7a19
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:10:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox убудова забяспечвае кантроль доступу, каб дазволіць карыстальнікам ствараць тэчкі сінхранізацыі хатняй дырэкторыі хмарнай службы Dropbox.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox палітыкі карыстальніка';
+$lang['user_dropbox_plugin_user'] = 'Dropbox Карыстальнік';
