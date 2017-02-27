0631c98847d5bd08ec5be5e4b79ceab13c374acf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:14:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox tappi veitir aðgangsstýringu til að leyfa notendum að búa til heimasvæði samstillingu við ský-undirstaða Dropbox þjónustu.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox User Stefna';
+$lang['user_dropbox_plugin_user'] = 'Dropbox User';
