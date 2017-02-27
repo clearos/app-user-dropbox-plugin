5ede7ed2321f11130948652e670292145bcb9a51
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:20:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Васлкунаки Dropbox таъмин назорати дастрасӣ ба иҷозат додан ба корвандон барои эҷоди як Серв феҳристи феҳристи хонагӣ ба хизматрасонии Dropbox асоси абр.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox сиёсати Корбар';
+$lang['user_dropbox_plugin_user'] = 'Dropbox корбар';
