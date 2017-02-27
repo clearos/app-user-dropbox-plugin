b3cec3a99ef111b82daf27b52914484ec26fcee1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:17:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox plugin gir adgangskontroll å tillate brukere å opprette en hjemmekatalog mappe synkronisert til den skybaserte Dropbox tjeneste.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Brukerbetingelser';
+$lang['user_dropbox_plugin_user'] = 'Dropbox Bruker';
