90f1e0511ad3bcd7d1380b0bcbcbf4629aa4cbec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:14:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropboxのプラグインは、ユーザーがクラウドベースのDropboxサービスにホームディレクトリのフォルダの同期を作成できるようにアクセス制御を提供します。';
+$lang['user_dropbox_plugin_app_name'] = 'Dropboxのユーザーポリシー';
+$lang['user_dropbox_plugin_user'] = 'Dropboxのユーザー';
