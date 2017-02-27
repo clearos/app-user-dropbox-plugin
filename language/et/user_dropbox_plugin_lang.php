880ae5b3d239474a75a06d45c8640a0611b74ab5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:12:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox plugin pakub juurdepääsu kontroll, mis võimaldab kasutajatel luua kodu kataloogikaust sync pilvepõhise Dropbox teenust.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Kasutaja Policy';
+$lang['user_dropbox_plugin_user'] = 'Dropbox Kasutaja';
