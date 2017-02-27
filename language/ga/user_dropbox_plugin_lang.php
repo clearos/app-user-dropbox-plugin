86bd88980ddc93fd942c18b9a74be43eeacf1c25
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:13:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Soláthraíonn Dropbox plugin rialaithe rochtana a ligeann d&#39;úsáideoirí a chruthú eolaire bhaile info fillteán don Dropbox seirbhís scamall-bhunaithe.';
+$lang['user_dropbox_plugin_app_name'] = 'Polasaí Úsáideora Dropbox';
+$lang['user_dropbox_plugin_user'] = 'Dropbox Úsáideora';
