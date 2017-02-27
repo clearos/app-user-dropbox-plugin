176ee72fb97d2c66ba12c0897dfe4acb89edb5f7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:12:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Demetuja modulo provizas alirkontrolo permesi uzantoj krei hejman dosierujon dosierujo sync al la nubo bazita demetuja servo.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Uzanto Politiko';
+$lang['user_dropbox_plugin_user'] = 'Dropbox Uzanto';
