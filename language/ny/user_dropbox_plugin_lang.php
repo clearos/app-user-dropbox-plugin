01a1aff483b7e0246a578165acb517e9f8792db1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:11:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox pulogalamu yowonjezera amapereka mwayi ulamuliro kulola owerenga kulenga nyumba Directory foda kulunzanitsa kuti mtambo ofotokoza Dropbox utumiki.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Policy Wosuta';
+$lang['user_dropbox_plugin_user'] = 'Dropbox Wosuta';
