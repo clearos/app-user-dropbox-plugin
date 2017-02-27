083bc2297f41479ae184b1e93e6f49c71286d178
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:16:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox приклучок обезбедува контрола на пристап за да се овозможи на корисниците да ја создаде папката синхронизација домашен директориум на облак-базирани Dropbox услуга.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Политика пристап';
+$lang['user_dropbox_plugin_user'] = 'Dropbox пристап';
