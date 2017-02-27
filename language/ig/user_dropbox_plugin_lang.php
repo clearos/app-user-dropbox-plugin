f0fb7bbcabe484a9a2a01aae5efb9b573304935a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:14:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox plugin enye ohere akara na-ekwe ka ndị ọrụ na-ike a home directory nchekwa mmekọrịta ka ígwé ojii nke dabeere na Dropbox ọrụ.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox User Policy';
+$lang['user_dropbox_plugin_user'] = 'Dropbox User';
