a3d071f47cb206afe845661d59c2e7fc97cf7af9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:17:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox plugin biedt toegangsbeheer waarmee gebruikers van een home directory map synchroniseren met de cloud-gebaseerde dienst Dropbox te maken.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Gebruikersverklaring';
+$lang['user_dropbox_plugin_user'] = 'Dropbox Gebruiker';
