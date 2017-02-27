79187eb0fc2e46a0b53fb47f820032cbbd3da8dc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:16:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox залгаас хэрэглэгчид үүл суурилсан Dropbox үйлчилгээ нь гэрийн сан хавтас синкийг бий болгох боломжийг олгох нь хандалтын хяналтыг олгодог.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Хэрэглэгчийн бодлого';
+$lang['user_dropbox_plugin_user'] = 'Dropbox-ын хэрэглэгчийн';
