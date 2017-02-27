c408c7fc0211baf28c7109535817824ab9b1314a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:13:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox modullal hozzáférés-szabályozás lehetővé teszi a felhasználók számára, a home könyvtár mappát szinkronban a felhő-alapú Dropbox szolgáltatást.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Felhasználási Feltételek';
+$lang['user_dropbox_plugin_user'] = 'Dropbox felhasználó';
