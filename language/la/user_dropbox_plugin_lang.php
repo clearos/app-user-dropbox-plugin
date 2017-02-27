f3f7bfa2a87a5977e626e56a8c53b733e9873627
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:15:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Plugin animadversae dat imperium accessum ad patitur users ad partum a folder domus presul ut sync per nubem-substructio servitium Dropbox.';
+$lang['user_dropbox_plugin_app_name'] = 'User dimittere Policy';
+$lang['user_dropbox_plugin_user'] = 'User dimittere';
