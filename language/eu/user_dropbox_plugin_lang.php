1a570f7ec6226e77c9345469fc1a0f350a0e23df
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:10:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox plugina sarbide kontrola ematen die erabiltzaileei home direktorioa karpetan hodeian oinarritutako Dropbox zerbitzua sinkronizazio bat sortu ahal izateko.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Erabiltzaile-politika';
+$lang['user_dropbox_plugin_user'] = 'Dropbox Erabiltzaileak';
