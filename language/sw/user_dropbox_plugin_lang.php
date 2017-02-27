6877688760d10541c0f5a17f0eda0b90f530c8f6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:19:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'Dropbox Plugin hutoa upatikanaji wa kudhibiti na kuruhusu watumiaji wa kujenga nyumba saraka folder upatanishi wa wingu makao Dropbox huduma.';
+$lang['user_dropbox_plugin_app_name'] = 'Dropbox Sera ya mtumiaji';
+$lang['user_dropbox_plugin_user'] = 'Dropbox mtumiaji';
