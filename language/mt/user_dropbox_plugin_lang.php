331eb1b118ed7a04d5a68a67b495f6d4a0b0a833
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 08:16:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/user_dropbox_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['user_dropbox_plugin_app_description'] = 'plugin Dropbox jipprovdi kontroll ta &#39;aċċess li jippermetti lill-utenti biex jinħoloq sinkronizzazzjoni folder direttorju dar għas-servizz sħaba bbażata fuq Dropbox.';
+$lang['user_dropbox_plugin_app_name'] = 'Politika Utent Dropbox';
+$lang['user_dropbox_plugin_user'] = 'Dropbox Utent';
